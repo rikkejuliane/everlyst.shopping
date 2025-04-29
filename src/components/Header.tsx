2b886d32bfb186e20e73b6b26d71c2095c527c21
rikkejuliane <rikand01864@stud.noroff.no>
2025-04-29 20:46:38 +0200

    had to fix tailwind (new version so new things to do) to figure out how to import fonts and colors. All good now

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="w-full">
+      <div>
+        <p className="font-(family-name:--font-afacad) font-bold text-xl">
+          Free shipping on orders over $50!
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header; // ✅ make sure you're exporting it as default!
