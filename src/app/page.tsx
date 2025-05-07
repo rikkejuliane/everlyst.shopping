bbd540cffc9e564e1eaf1b92361a13eca5c8ac6f
rikkejuliane <rikand01864@stud.noroff.no>
2025-05-07 13:21:38 +0200

    added the sorting function to the products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import SortDropdown from "@/components/SortDropdown";
 import ProductList from "@/components/ProductList";
 import { fetchAllProducts } from "@/utils/api";
 import { Product } from "@/types/products";
@@ -10,6 +11,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [sortOption, setSortOption] = useState("relevance");
 
   useEffect(() => {
     const load = async () => {
@@ -23,14 +25,29 @@ export default function Home() {
     p.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    switch (sortOption) {
+      case "low-high":
+        return a.price - b.price;
+      case "high-low":
+        return b.price - a.price;
+      case "az":
+        return a.title.localeCompare(b.title);
+      case "za":
+        return b.title.localeCompare(a.title);
+      default:
+        return 0;
+    }
+  });
+
   return (
-    <main className="flex flex-col items-center justify-center bg-background min-h-screen">
-        <Hero />
-        <div className="flex flex-row justify-between mb-2">
-          <SearchBar value={searchQuery} onChange={setSearchQuery} />
-          
-        </div>
-        <ProductList products={filteredProducts} />
+    <main className="flex flex-col items-center justify-center bg-background">
+      <Hero />
+      <div className="flex flex-row justify-between mb-2 w-3/4">
+        <SearchBar value={searchQuery} onChange={setSearchQuery} />
+        <SortDropdown value={sortOption} onChange={setSortOption} />
+      </div>
+      <ProductList products={sortedProducts} />
     </main>
   );
 }
